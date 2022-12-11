13aedc719d32de568dccf3f80ddaadf6ffdb0f6e
kardibus <kardibus@gmail.com>
2022-12-11 22:11:35 +0500

    Создан телеграм бот

--- src/main/kotlin/com/kardibus/pet/WordsService.kt
+++ src/main/kotlin/com/kardibus/pet/WordsService.kt
@@ -2,19 +2,16 @@ package com.kardibus.pet
 
 import com.elbekd.bot.Bot
 import com.elbekd.bot.model.toChatId
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 
 @Service
-class WordsService( @Value("token") private  var token:String,private var wordsRepository: WordsRepository) {
+class WordsService(private var wordsRepository: WordsRepository) {
 
     init {
-
-
+        val token = "5903504093:AAGEWOrT2M-E6KLkjKAIchwMu8hwNx9s-Yk"
         val bot = Bot.createPolling(token)
-
         bot.onMessage { msg ->
-            if (wordsRepository.findByWordOutInt(msg.text.toString())>0) {
+            if (wordsRepository.findByWordOutInt(msg.text.toString()) > 0) {
 
                 bot.sendMessage(msg.chat.id.toChatId(), text = "у нас нельзя матерится в чате")
                 return@onMessage
@@ -27,25 +24,13 @@ class WordsService( @Value("token") private  var token:String,private var wordsR
                 )
             }
 
-            if (msg.leftChatMember?.id!=null) {
+            if (msg.leftChatMember?.id != null) {
                 bot.sendMessage(
                     msg.chat.id.toChatId(),
                     "Прощай ${msg.leftChatMember?.first_name} ${msg.leftChatMember?.lastName}"
                 )
             }
         }
-
-//        bot.chain(
-//            label = "new_chat_members",
-//            predicate = { msg -> !msg.newChatMembers.isNullOrEmpty() },
-//            action = { msg ->
-//                if (!msg.newChatMembers.isNullOrEmpty()) {
-//                    bot.sendMessage(
-//                        msg.chat.id.toChatId(),
-//                        "Приветствуем тебя в чате ${msg.newChatMembers.first().first_name} ${msg.newChatMembers.first().lastName}"
-//                    )
-//                }
-//            }).build()
 
         bot.start()
     }
